c15b941894a4bfa5b29559819387d87bc313c66f
AsP3X <development@vorberg-webmail.de>
2023-01-18 10:36:42 +0100

    TASK: replaced old if user exist with new check

--- routes/login.js
+++ routes/login.js
@@ -5,31 +5,72 @@ const fs = require('fs');
 
 // Loading custom modules
 const Logger = require('../assets/utils/logger');
-const { DBConnector } = require('../assets/database/DBManager');
+const {
+  DBConnector
+} = require('../assets/database/DBManager');
 
 // Loading models
 const userSchema = require('../assets/models/user');
 
 async function retrieveUser(username, email) {
-    let user = null;
-    if (username) {
-        user = await userSchema.findOne({ username: username});
-        return user;
-    }
+  let user = null;
+  if (username) {
+    user = await userSchema.findOne({
+      username: username
+    });
 
-    if (email) {
-        user = await userSchema.findOne({ email: email });
-        return user;
+    if (!user) {
+      return null;
     }
+    return user;
+  }
+
+  if (email) {
+    user = await userSchema.findOne({
+      email: email
+    });
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
 }
 
 async function retreivePasswordHash(username) {
-    const user = await userSchema.findOne({ username: username });
-    return user.password;
+  const user = await userSchema.findOne({
+    username: username
+  });
+  return user.password;
+}
+
+async function checkIfStatus(username) {
+  const user = await userSchema.findOne({
+    username: username
+  })
+
+  if (user.status === "active") {
+    return {
+      username: username,
+      status: "active",
+    };
+  } else {
+    return {
+      username: username,
+      status: user.status,
+    };
+  }
+}
+
+async function setLastLogin(username) {
+  const user = await userSchema.findOne({
+    username: username
+  });
+  user.lastLogin = Date.now();
+  await user.save();
 }
 
 function checkPasswordHash(incPassword, dbPassword) {
-    return incPassword === dbPassword;
+  return incPassword === dbPassword;
 }
 
 class Health {
@@ -57,40 +98,56 @@ class Health {
   rootRoute() {
     this.logger.log("Loading root route...");
     this.router.post("/", async (req, res) => {
-        const {username, email, password} = req.body;
+      const {
+        username,
+        email,
+        password
+      } = req.body;
 
-        // check if at least one of these fields are filled [username, email]
-        if (!username && !email) {
-            res.status(400).json({
-                error: "Missing fields",
-                message: "Please fill in all the fields",
-                fields: ["username", "email"]
-            });
-            return;
+      // check if at least one of these fields are filled [username, email]
+      if (!username && !email) {
+        res.status(400).json({
+          error: "Missing fields",
+          message: "Please fill in all the fields",
+          fields: ["username", "email"]
+        });
+        return;
+      }
+
+      const user = await retrieveUser(username, email);
+
+      // check if user exists
+      if (!user) {
+        res.status(404).json({
+          error: "User not found",
+          message: "Please check your username and password or create a new account"
+        });
+        return;
+      }
+
+      // check if users password maches the one in the database
+      const userStatus = await checkIfStatus(user.username);
+      console.log(userStatus.status);
+      if (userStatus.status !== "active") {
+        res.status(403).json({
+          user: userStatus.username,
+          status: userStatus.status,
+          message: "User is not active, please contact the administrator",
+        });
+        return;
+      }
+
+      await setLastLogin(user.username);
+      res.status(200).json({
+        message: "User found",
+        user: {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          creationDate: user.creationDate,
+          lastLogin: user.lastLogin
         }
-
-        const user = await retrieveUser(username, email);
-
-        // check if users password maches the one in the database
-        
-
-        if (user) {
-            res.status(200).json({
-                message: "User found",
-                user: {
-                    id: user._id,
-                    username: user.username,
-                    email: user.email,
-                    creationDate: user.creationDate,
-                    lastLogin: user.lastLogin
-                }
-            });
-        } else {
-            res.status(404).json({
-                error: "User not found",
-                message: "User not found"
-            });
-        }
+      });
     });
   }
 
